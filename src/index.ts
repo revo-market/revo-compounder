95623709b7371255e676341e0c3fc04b990aa79c
Charlie Andrews <charlie.andrews@valoraapp.com>
2022-04-13 19:57:46 +0200

    fix: send response code to terminate cloud function

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,36 @@ import { FARM_BOTS, NODE_URLS } from './constants'
 import { getCompounderConfig } from './config'
 import { compound } from './compound'
 import { getKit } from './kit'
+import { HttpFunction } from '@google-cloud/functions-framework/build/src/functions'
 
-export async function main(): Promise<number> {
-  const {
-    privateKey,
-    gas,
-    gasPrice,
-    deadlineSecondsAhead,
-    network,
-    farmBotNames,
-  } = await getCompounderConfig()
-  const { kit, address: compounderAddress } = await getKit(
-    privateKey,
-    NODE_URLS[network],
-  )
-
-  for (const farmBotName of farmBotNames) {
-    const farmBotConfig = FARM_BOTS[network][farmBotName]
-    await compound({
-      kit,
-      farmBotConfig,
-      compounderAddress,
+export const main: HttpFunction = async (_req, res) => {
+  try {
+    const {
+      privateKey,
       gas,
       gasPrice,
       deadlineSecondsAhead,
-    })
+      network,
+      farmBotNames,
+    } = await getCompounderConfig()
+    const { kit, address: compounderAddress } = await getKit(
+      privateKey,
+      NODE_URLS[network],
+    )
+
+    for (const farmBotName of farmBotNames) {
+      const farmBotConfig = FARM_BOTS[network][farmBotName]
+      await compound({
+        kit,
+        farmBotConfig,
+        compounderAddress,
+        gas,
+        gasPrice,
+        deadlineSecondsAhead,
+      })
+    }
+    res.status(200).send({})
+  } catch (e) {
+    res.status(500).send({error: e})
   }
-  return 1 // google cloud function needs this
 }
